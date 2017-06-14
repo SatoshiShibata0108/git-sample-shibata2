ae973ab72b52ce55396e018d44823da9921b33ca
satoshi.shiabta <satoshi.shiabta@rakus.co.jp>
2017-06-14 10:50:56 +0900

    新規プロジェクト

---
+++ src/main/java/com/example/demo/GitSample2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSample2Application.class, args);
+	}
+}
